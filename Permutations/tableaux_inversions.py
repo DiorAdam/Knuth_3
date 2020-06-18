ecdc4046517a330884b07090ac8fa311f12f8a1d
fadel <fadelsougou@gmail.com>
2020-06-18 14:58:56 +0200

    fix(Permutations/tableaux and involutions)

--- Permutations/tableaux_inversions.py
+++ Permutations/tableaux_inversions.py
@@ -1,5 +1,8 @@
 
 def insertion(tab, val):
+    if not tab:
+        tab.append([val])
+        return
     i, j = 0, len(tab[0])
     while i < len(tab):
         j = min(j, len(tab[i])-1)
@@ -11,6 +14,9 @@ def insertion(tab, val):
         else:
             tab[i][j+1], val = val, tab[i][j+1]
         i+=1
+
+    if i==len(tab):
+        tab.append([val])
 
 def deletion(tab, s):
     i, j = s, len(tab[s])
@@ -25,12 +31,14 @@ def deletion(tab, s):
         i-=1
 
 
-'''
 t = [[1,3,5,9,12,16], [2,6,10,15], [4,13,14], [11], [17]]
-insertion(t, 8)
-deletion(t,3)
-deletion(t,3)
+t = []
+p = [7,2,9,5,3]
+for x in p:   
+    insertion(t,x)
 
+#deletion(t,3)
+#deletion(t,3)
 for x in t:
     print(x)
-'''
+
