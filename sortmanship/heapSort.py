e069fc4c57207b7ed40c1c7d48de23265e32047f
fadel <fadelsougou@gmail.com>
2020-07-06 12:19:11 +0200

    heapSort with SiftUp Algorithm

--- sortmanship/heapSort.py
+++ sortmanship/heapSort.py
@@ -13,7 +13,6 @@ def heapify(t):
                 t[i-1], t[j-1] = t[j-1], t[i-1]
                 i = j
             else: break
-    return t
     
 
 
@@ -26,33 +25,27 @@ def isHeap(t):
 
 def heapSort(t):
     heapify(t)
-    print(t)
     n = len(t)
     r = n-1
     while r > 0:
-        t[0], t[r] = t[r], t[0]
-        t[:r] = heapify(t[:r])
-        '''
-        a = t[0]
-        k = 1
-        while 2*k <= r:
-            if t[2*k-1] > t[2*k]:
-                t[k-1] = t[2*k-1]
-                k = 2*k
-            else:
-                t[k-1] = t[2*k]
-                k = 2*k+1
-        for i in range(k,r+1):
-            t[i-1],t[i] = t[i], t[i-1]
-        t[r] = a
-        print(t, isHeap(t[:r]))
-        '''
+        # SiftUp algorithm #
+        K = t[r]                                     
+        t[r] = t[0]
         r-=1
+        j = 1
+        while True:                         
+            i, j = j, 2*j
+            if j -1 < r and t[j] > t[j-1]:
+                j+=1
+            if j-1>r or K >= t[j-1]:
+                t[i-1] = K
+                break
+            t[i-1] = t[j-1] 
+
             
 
     
 t = [503, 87, 512, 61, 908, 170, 897, 275, 653, 426, 154, 509, 612, 677, 765, 703]
-
 heapSort(t)
 print(t)
 
