71d048fac1fe237435fbd40f6e3fa950d8fb8ec1
fadel <fadelsougou@gmail.com>
2020-07-06 02:12:49 +0200

    heapSort premices

--- sortmanship/heapSort.py
+++ sortmanship/heapSort.py
@@ -13,6 +13,7 @@ def heapify(t):
                 t[i-1], t[j-1] = t[j-1], t[i-1]
                 i = j
             else: break
+    return t
     
 
 
@@ -21,11 +22,38 @@ def isHeap(t):
         if t[k-1] > t[k//2-1]:
             return False
     return True
+
+
+def heapSort(t):
+    heapify(t)
+    print(t)
+    n = len(t)
+    r = n-1
+    while r > 0:
+        t[0], t[r] = t[r], t[0]
+        t[:r] = heapify(t[:r])
+        '''
+        a = t[0]
+        k = 1
+        while 2*k <= r:
+            if t[2*k-1] > t[2*k]:
+                t[k-1] = t[2*k-1]
+                k = 2*k
+            else:
+                t[k-1] = t[2*k]
+                k = 2*k+1
+        for i in range(k,r+1):
+            t[i-1],t[i] = t[i], t[i-1]
+        t[r] = a
+        print(t, isHeap(t[:r]))
+        '''
+        r-=1
+            
+
     
 t = [503, 87, 512, 61, 908, 170, 897, 275, 653, 426, 154, 509, 612, 677, 765, 703]
 
-heapify(t)
-print(isHeap(t))
+heapSort(t)
 print(t)
 
 
